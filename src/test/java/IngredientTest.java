a8ca5a9fb73728c5e5d2ac6086443c790ec33eaf
natapis <natapis2009@mail.ru>
2023-09-07 20:40:54 +0300

    IngredientTest add

---
+++ src/test/java/IngredientTest.java
@@ -0,0 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+public class IngredientTest {
+    private IngredientType typeIngredient = IngredientType.SAUCE;
+    private String nameIngredient = "Соус традиционный галактический";
+    private float priceIngredient = 15;
+
+    @Test
+    public void getNameIngredientTest(){
+        Ingredient ingredient = new Ingredient(typeIngredient, nameIngredient, priceIngredient);
+        String actualName = ingredient.getName();
+        Assert.assertEquals("Имена ингредиентов не совпадают", nameIngredient, actualName);
+    }
+    @Test
+    public void getPriceIngredientTest(){
+        Ingredient ingredient = new Ingredient(typeIngredient, nameIngredient, priceIngredient);
+        float actualPrice = ingredient.getPrice();
+        Assert.assertEquals(priceIngredient, actualPrice, 0);
+    }
+    @Test
+    public void getTypeIngredientTest() {
+        Ingredient ingredient = new Ingredient(typeIngredient, nameIngredient, priceIngredient);
+        IngredientType actualType = ingredient.getType();
+        Assert.assertEquals(typeIngredient, actualType);
+    }
+    }
