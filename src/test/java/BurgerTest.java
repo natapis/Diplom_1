e1b99bf235a530a61c936c33df0922ec0ffada95
natapis <natapis2009@mail.ru>
2023-09-07 22:26:33 +0300

    BurgerTest add test for moveIngredient/removeIngredient

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -9,18 +9,20 @@ import praktikum.IngredientType;
 public class BurgerTest {
 
     private Ingredient ingredient = new Ingredient(IngredientType.FILLING,"Test", 203);
+    private Ingredient ingredientTwo = new Ingredient(IngredientType.SAUCE,"Test2", 209);
     private Bun bun = new Bun("TestBun", 567);
     @Test
     public void addIngredientTest(){
         Burger burger = new Burger();
         burger.addIngredient(ingredient);
-        IngredientType actualType = burger.ingredients.get(0).getType();
-        String actualName = burger.ingredients.get(0).getName();
-        float actualPrice = burger.ingredients.get(0).getPrice();
-        Assert.assertEquals(203, actualPrice, 0);
-        Assert.assertEquals("Не добавилось имя", "Test", actualName);
-        Assert.assertEquals(IngredientType.FILLING, actualType);
+ //       IngredientType actualType = burger.ingredients.get(0).getType();
+ //       String actualName = burger.ingredients.get(0).getName();
+ //       float actualPrice = burger.ingredients.get(0).getPrice();
+ //       Assert.assertEquals(203, actualPrice, 0);
+ //       Assert.assertEquals("Не добавилось имя", "Test", actualName);
+//        Assert.assertEquals(IngredientType.FILLING, actualType);
         Assert.assertEquals(false, burger.ingredients.isEmpty());
+        Assert.assertEquals(true,burger.ingredients.contains(ingredient));
     }
 
     @Test
@@ -31,6 +33,31 @@ public class BurgerTest {
         float actualPrice = burger.bun.getPrice();
         Assert.assertEquals(567, actualPrice,0);
         Assert.assertEquals("TestBun", actualName);
-        ;
+    }
+    @Test
+    public void removeIngredientTest(){
+        Burger burger = new Burger();
+        burger.addIngredient(ingredient);
+//        int size = burger.ingredients.size();
+//        if (size != 0){
+            burger.ingredients.remove(0);
+//        }
+//        int actualSize = burger.ingredients.size();
+//        Assert.assertEquals(size-1, actualSize, 0);
+        Assert.assertEquals(false, burger.ingredients.contains(ingredient));
+    }
+
+    @Test
+    public void moveIngredientTest(){
+        Burger burger = new Burger();
+        burger.addIngredient(ingredient);
+        burger.addIngredient(ingredientTwo);
+        int actualSize = burger.ingredients.size();
+        burger.moveIngredient(0,1);
+        int actualIndexOne = burger.ingredients.indexOf(ingredient);
+        int actualIndexTwo = burger.ingredients.indexOf(ingredientTwo);
+        Assert.assertEquals(1, actualIndexOne, 0);
+        Assert.assertEquals(0, actualIndexTwo, 0);
+        Assert.assertEquals(2, actualSize, 0);
     }
 }
