2cb46f40b1eeb1b3b16006b273ce672dbe7706bb
natapis <natapis2009@mail.ru>
2023-09-07 21:34:31 +0300

    BurgerTest add test for setBuns

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,13 +1,15 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import praktikum.Bun;
 import praktikum.Burger;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
 public class BurgerTest {
 
-    Ingredient ingredient = new Ingredient(IngredientType.FILLING,"Test", 203);
+    private Ingredient ingredient = new Ingredient(IngredientType.FILLING,"Test", 203);
+    private Bun bun = new Bun("TestBun", 567);
     @Test
     public void addIngredientTest(){
         Burger burger = new Burger();
@@ -19,5 +21,16 @@ public class BurgerTest {
         Assert.assertEquals("Не добавилось имя", "Test", actualName);
         Assert.assertEquals(IngredientType.FILLING, actualType);
         Assert.assertEquals(false, burger.ingredients.isEmpty());
+    }
+
+    @Test
+    public void setBunsTest(){
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        String actualName = burger.bun.getName();
+        float actualPrice = burger.bun.getPrice();
+        Assert.assertEquals(567, actualPrice,0);
+        Assert.assertEquals("TestBun", actualName);
+        ;
     }
 }
