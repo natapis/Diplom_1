6d4045ddff69ce6d644eedc74de520b26b5ff08c
natapis <natapis2009@mail.ru>
2023-09-07 20:15:14 +0300

    BunTest убрала параметризацию

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,48 +1,22 @@
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import praktikum.Bun;
-@RunWith(Parameterized.class)
+
 public class BunTest {
-    private final String expectedName;
-    private final float expectedPrice;
-    private final String actualName;
-    private final float actualPrice;
     private final String NAME = "Burger";
     private final float PRICE = 12;
-    public BunTest(String name, String expectedName){
-        this.actualName = name;
-        this.expectedName = expectedName;
-        this.expectedPrice = PRICE;
-        this.actualPrice = PRICE;
+
+    @Test
+    public void getNameTest() {
+        Bun bun = new Bun(NAME, PRICE);
+        String actualName = bun.getName();
+        Assert.assertEquals("Неверное имя булочки", NAME, actualName);
     }
 
-    public BunTest(float price, float expectedPrice){
-        this.expectedPrice = expectedPrice;
-        this.actualPrice = price;
-        this.expectedName = NAME;
-        this.actualName = NAME;
+    @Test
+    public void getPriceTest() {
+        Bun bun = new Bun(NAME, PRICE);
+        float actualPrice = bun.getPrice();
+        Assert.assertEquals(PRICE, actualPrice, 0);
     }
-    @Parameterized.Parameters
-    public static Object[][] bunTest(){
-        return new Object[][]{
-                {"Lopast", "Lopast"},
-                {12,12},
-        };
-    }
-
-@Test
-public void getNameTest(){
-    Bun bun = new Bun(actualName, PRICE);
-    String actualName = bun.getName();
-    Assert.assertEquals("Неверное имя булочки", expectedName, actualName);
-}
-
-@Test
-    public void getPriceTest(){
-    Bun bun = new Bun(NAME, actualPrice);
-    float actualPrice = bun.getPrice();
-    Assert.assertEquals(expectedPrice, actualPrice, 0);
-}
 }
