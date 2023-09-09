150862efd24fcc94462fa89446d6955698940f81
natapis <natapis2009@mail.ru>
2023-09-09 22:10:42 +0300

    BurgerMoveIngredientParametersTest add assert ismove

--- src/test/java/BurgerMoveIngredientParametersTest.java
+++ src/test/java/BurgerMoveIngredientParametersTest.java
@@ -1,5 +1,4 @@
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -13,16 +12,16 @@ public class BurgerMoveIngredientParametersTest {
     private int index;
     private int newIndex;
     boolean isMove;
-//    private float expectedSize;
-//    private float expectedIndex;
-    public BurgerMoveIngredientParametersTest(int sizeOne, int index, int newIndex, boolean isMove){
+
+    public BurgerMoveIngredientParametersTest(int sizeOne, int index, int newIndex, boolean isMove) {
         this.sizeIngredientMassive = sizeOne;
         this.index = index;
         this.newIndex = newIndex;
         this.isMove = isMove;
     }
-    @Parameterized.Parameters (name="Перемещение в массиве размером {0} c {1}-го на {2}-е место = {3}")
-    public static Object[][] burgerMoveIngredient(){
+
+    @Parameterized.Parameters(name = "Перемещение в массиве размером {0} c {1}-го на {2}-е место = {3}")
+    public static Object[][] burgerMoveIngredient() {
         return new Object[][]{
                 {1, 0, 1, false},
                 {2, 0, 1, true},
@@ -33,29 +32,24 @@ public class BurgerMoveIngredientParametersTest {
                 {3, 2, 0, true},
         };
     }
-//    @Before
 
     @Test
-    public void moveIngredientTest(){
- //       int index = 1;
- //       int indexNew = 3;
-        if (sizeIngredientMassive >1) {
+    public void moveIngredientTest() {
+        if (sizeIngredientMassive > 1) {
             Burger burger = new Burger();
-            for (int i = 0; i <= sizeIngredientMassive-1; i++) {
+            for (int i = 0; i <= sizeIngredientMassive - 1; i++) {
                 Ingredient ingredientTest = new Ingredient(IngredientType.FILLING, "Test" + i, 23 + i);
                 burger.addIngredient(ingredientTest);
             }
-            //       burger.addIngredient(ingredient);
-            //       burger.addIngredient(ingredientTwo);
             int actualSize = burger.ingredients.size();
             Ingredient ingredient = burger.ingredients.get(index);
             burger.moveIngredient(index, newIndex);
             int actualIndex = burger.ingredients.indexOf(ingredient);
-//        int actualIndexTwo = burger.ingredients.indexOf(ingredientTwo);
-            isMove = ((newIndex==actualIndex)&&(sizeIngredientMassive==actualSize));
-            //       Assert.assertEquals(0, actualIndexTwo, 0);
-            Assert.assertEquals(sizeIngredientMassive, actualSize, 0);
-        } else {System.out.println("Move impossible");
-            isMove = false;}
+            isMove = ((newIndex == actualIndex) && (sizeIngredientMassive == actualSize));
+            Assert.assertEquals("Индексы не совпадают", isMove, true);
+        } else {
+            System.out.println("Move impossible");
+            isMove = false;
+        }
     }
 }
