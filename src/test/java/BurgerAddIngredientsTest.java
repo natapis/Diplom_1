776c1ba7dc47e49678859ec2f330aa27795d2f3f
natapis <natapis2009@mail.ru>
2023-09-17 22:54:34 +0300

    add mock

--- src/test/java/BurgerAddIngredientsTest.java
+++ src/test/java/BurgerAddIngredientsTest.java
@@ -1,15 +1,27 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import praktikum.Burger;
 import praktikum.Database;
 import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyInt;
 
 @RunWith(Parameterized.class)
 public class BurgerAddIngredientsTest {
     int sizeIngredientMassive;
     boolean isAdd;
+    @Mock
+    Database data;
 
     public BurgerAddIngredientsTest(int size, boolean isAdd) {
         this.sizeIngredientMassive = size;
@@ -23,13 +35,20 @@ public class BurgerAddIngredientsTest {
                 {1, true},
         };
     }
+    @Before
+    public void init(){
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     public void addIngredientTest() {
         Burger burger = new Burger();
-        Database data = new Database();
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "hot sauce", 100);
+        List<Ingredient> list = new ArrayList<>();
+        Mockito.when(data.availableIngredients()).thenReturn(list);
         if (sizeIngredientMassive >= 0) {
             for (int i = 0; i <= sizeIngredientMassive; i++) {
+                list.add(ingredient);
                 Ingredient ingredientTest = data.availableIngredients().get(i);
                 burger.addIngredient(ingredientTest);
             }
